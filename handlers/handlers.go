c5beec2494ca800d788e3ef64a6dacaa8f4a568a
Matt Smith <96mattsmith@gmail.com>
2019-02-13 10:45:29 -0700

    commit

---
+++ handlers/handlers.go
@@ -0,0 +1,74 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/byuoitav/atlona-switcher-microservice/helpers"
+	"github.com/byuoitav/common/log"
+	"github.com/byuoitav/common/status"
+	"github.com/byuoitav/common/structs"
+	"github.com/labstack/echo"
+)
+
+//TODO create global variable for last time the power was reset
+//Have each function check the time to see if it needs to be reset again
+func SwitchInput(context echo.Context) error {
+	output := context.Param("output")
+
+	outport, _ := strconv.Atoi(output)
+	outport = outport + 1
+
+	input := context.Param("input")
+
+	inport, _ := strconv.Atoi(input)
+	inport = inport + 1
+
+	address := context.Param("address")
+
+	resp, err := helpers.SwitchInput(address, fmt.Sprintf("%v", outport), fmt.Sprintf("%v", inport))
+	if err != nil {
+		log.L.Errorf("Failed to establish connection with %s : %s", address, err.Error())
+		return context.JSON(http.StatusInternalServerError, err)
+	}
+
+	//decrement response by 1
+	response, _ := strconv.Atoi(resp)
+	response = response - 1
+	//in:out
+	return context.JSON(http.StatusOK, status.Input{Input: fmt.Sprintf("%v:%v", input, output)})
+}
+
+func ShowOutput(context echo.Context) error {
+	address := context.Param("address")
+
+	inport, outport, err := helpers.GetOutput(address)
+	if err != nil {
+		log.L.Errorf("Failed to establish connection with %s : %s", address, err.Error())
+		return context.JSON(http.StatusInternalServerError, err)
+	}
+	input, _ := strconv.Atoi(inport)
+	input = input - 1
+	log.L.Infof("input: %d", input)
+	output, _ := strconv.Atoi(outport)
+	log.L.Infof("ouput: %d", output)
+
+	return context.JSON(http.StatusOK, status.Input{Input: fmt.Sprintf("%v:%v", input, output)})
+}
+
+func HardwareInfo(context echo.Context) error {
+	address := context.Param("address")
+	ipaddr, macaddr, verdata, err := helpers.GetHardware(address)
+	if err != nil {
+		log.L.Errorf("Failed to establish connection with %s : %s", address, err.Error())
+		return context.JSON(http.StatusInternalServerError, err)
+	}
+	return context.JSON(http.StatusOK, structs.HardwareInfo{
+		NetworkInfo: structs.NetworkInfo{
+			IPAddress:  ipaddr,
+			MACAddress: macaddr,
+		},
+		FirmwareVersion: verdata,
+	})
+}
---
+++ helpers/get.go
@@ -0,0 +1,145 @@
+package helpers
+
+import (
+	"fmt"
+	"net"
+	"strings"
+
+	"github.com/byuoitav/common/log"
+	"github.com/byuoitav/common/nerr"
+)
+
+//This function returns the current input that is being shown as the output
+func GetOutput(address string) (string, string, *nerr.E) {
+	conn, err := getConnection(address, true)
+	if err != nil {
+		log.L.Errorf("Failed to establish connection with %s : %s", address, err.Error())
+		return "", "", nerr.Translate(err).Add("Telnet connection failed")
+	}
+	//close connection
+	defer conn.Close()
+	status := getPowerStatus(conn)
+	if status != "ON" {
+		return "", "", nerr.Create("Cannot turn device on", "")
+	}
+	log.L.Infof("Power status: %s", status)
+
+	conn.Write([]byte(fmt.Sprintf("Status\r\n")))
+	b, err := readUntil(CARRIAGE_RETURN, conn, 10)
+	if err != nil {
+		return "", "", nerr.Translate(err).Add("failed to read from connection")
+	}
+
+	response := strings.Split(fmt.Sprintf("%s", b), "AV")
+
+	log.L.Infof("response: '%s'", response[0])
+	log.L.Infof("response: '%s'", response[1])
+	input := string(response[0])
+	input = input[len(input)-1:]
+	output := string(response[1])
+	output = output[1:]
+
+	log.L.Infof("input: '%s'", input)
+	log.L.Infof("output: '%s'", output)
+
+	return fmt.Sprintf("%s", input), fmt.Sprintf("%s", output), nil
+}
+
+//This function gets the IP Address (ipaddr), Software and hardware
+//version (verdata), and mac address (macaddr) of the device
+func GetHardware(address string) (string, string, string, *nerr.E) {
+	conn, gerr := getConnection(address, true)
+	if gerr != nil {
+		log.L.Errorf("Failed to get connection with %s: %s", address, gerr.Error())
+		return "", "", "", nerr.Translate(gerr).Add("Telnet connection failed")
+	}
+	//close connection
+	defer conn.Close()
+
+	status := getPowerStatus(conn)
+	if status != "ON" {
+		return "", "", "", nerr.Create("Cannot turn device on", "")
+	}
+	log.L.Infof("Power status: %s", status)
+
+	ipaddr, err := getIPAddress(address, conn)
+	if err != nil {
+		log.L.Errorf("Failed to establish connection with %s : %s", address, err.Error())
+		return "", "", "", err.Add("Telnet connection failed")
+	}
+
+	verdata, err := getVerData(address, conn)
+	if err != nil {
+		log.L.Errorf("Failed to establish connection with %s : %s", address, err.Error())
+		return "", "", "", err.Add("Telnet connection failed")
+	}
+
+	macaddr, err := getMacAddress(address, conn)
+	if err != nil {
+		log.L.Errorf("Failed to establish connection with %s : %s", address, err.Error())
+		return "", "", "", err.Add("Telnet connection failed")
+	}
+
+	conn.Close()
+	return ipaddr, macaddr, verdata, nil
+}
+
+func getIPAddress(address string, conn *net.TCPConn) (string, *nerr.E) {
+	conn.Write([]byte("IPCFG\r\n"))
+	b, err := readUntil(LINE_FEED, conn, 10)
+	if err != nil {
+		return "", nerr.Translate(err).Add("failed to read IP address from connection")
+	}
+	response := strings.Split(string(b), "IP Addr: ")
+	ipaddr := strings.Split(response[1], "Netmask")
+	ipaddr[0] = strings.TrimSpace(ipaddr[0])
+	log.L.Infof("IP address: %s", ipaddr[0])
+	return ipaddr[0], nil
+}
+
+//gets software and hardware data
+func getVerData(address string, conn *net.TCPConn) (string, *nerr.E) {
+	conn.Write([]byte("Version\r\n"))
+	log.L.Info("Just wrote the command for version")
+	b, err := readUntil(LINE_FEED, conn, 10)
+	if err != nil {
+		return "", nerr.Translate(err).Add("failed to read VerData from connection")
+	}
+	verdata := fmt.Sprintf("%s", b)
+	verdata = strings.TrimSpace(verdata)
+
+	log.L.Infof("version: %s", verdata)
+	return verdata, nil
+}
+
+//gets macaddress of device
+func getMacAddress(address string, conn *net.TCPConn) (string, *nerr.E) {
+	conn.Write([]byte("RAtlMac\r\n"))
+	b, err := readUntil(CARRIAGE_RETURN, conn, 10)
+	if err != nil {
+		return "", nerr.Translate(err).Add("failed to read Mac Address from connection")
+	}
+	macaddr := fmt.Sprintf("%s", b)
+	macaddr = strings.TrimSpace(macaddr)
+
+	log.L.Infof("macaddress: %s", macaddr)
+	return macaddr, nil
+}
+
+//check to make sure that the device is awake
+func getPowerStatus(conn *net.TCPConn) string {
+	conn.Write([]byte("PWSTA\r\n"))
+	b, _ := readUntil(CARRIAGE_RETURN, conn, 10)
+	status := ""
+	log.L.Infof("Response: %s", b)
+	test := strings.Split(fmt.Sprintf("%s", b), "PW")
+	log.L.Infof("Split: %s", test[1])
+
+	if test[1] != "ON" {
+		conn.Write([]byte("PWON\r\n"))
+		status = "ON"
+	} else {
+		status = "ON"
+	}
+	return status
+}
